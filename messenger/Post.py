a61e703b9a2aae4ef6a393242b2855eebc612c70
Grandik <theonlylimitisimagination@gmail.com>
2024-05-28 22:48:27 +0300

    Initial commit.

---
+++ messenger/Post.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from datetime import datetime
+
+
+# # Абстрактный класс, описывающий общие методы для всех постов.
+# class IPost(ABC):
+#     def 
+
+
+class Post:
+    def __init__(self):
+        
